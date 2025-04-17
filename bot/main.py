e95138c7551cae245a477e557924a0f8f5bf8d40
Oleg Fedotov <ol.fed25@gmail.com>
2025-04-17 22:26:30 +0300

    add telegram bot

---
+++ bot/main.py
@@ -0,0 +1,18 @@
+from os import getenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+TOKEN = getenv('TOKEN')
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text="Привет. Это WeatherGoidaBot")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
